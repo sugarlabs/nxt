232ec3bf1da02503c58b0edc4df7c88ef3da45ce
Alan Aguiar <alanjas@hotmail.com>
2012-01-30 12:36:06 -0200

    arreglo error minimo

--- nxt_plugin.py
+++ nxt_plugin.py
@@ -40,7 +40,7 @@ import sys
 sys.path.insert(0, os.path.abspath('./plugins/nxt_plugin'))
 import nxt
 from nxt.locator import BrickNotFoundError
-from nxt.locator import Method()
+from nxt.locator import Method
 from nxt.motor import PORT_A, PORT_B, PORT_C, Motor, SynchronizedMotors
 from nxt.sensor import PORT_1, PORT_2, PORT_3, PORT_4, Touch, Color20, \
      Ultrasonic, Type
